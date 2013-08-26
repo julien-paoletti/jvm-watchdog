6c9109953103c49296659dba3a41fca4131f4b70
julien-paoletti <julien.paoletti@gmail.com>
2013-08-26 23:45:26 +0200

    Heart beat sending skeleton

--- src/main/java/org/javabenchmark/jvmwatchdog/JvmWatchDogAgent.java
+++ src/main/java/org/javabenchmark/jvmwatchdog/JvmWatchDogAgent.java
@@ -3,13 +3,23 @@ package org.javabenchmark.jvmwatchdog;
 import java.lang.instrument.Instrumentation;
 
 /**
- *
+ * A java agent that is dynamically injected into JVM monitored by the watch dog.
+ * Once loaded, this agent sends <i>heart beat</i> to the watch dog. An heart beat
+ * is a JSON message that holds few data.
  * @author julien.paoletti@gmail.com
  */
 public class JvmWatchDogAgent {
 
+    /**
+     * starts the heart beat generator.
+     * @param agentArgs the agent args
+     * @param inst the instrumentation component
+     */
     public static void agentmain(String agentArgs, Instrumentation inst) {
 
-        System.out.println("Hey, the agent was dynamically loaded !");
+        HeartBeatGenerator generator = new HeartBeatGenerator();
+        generator.start();
+        
     }
+    
 }
